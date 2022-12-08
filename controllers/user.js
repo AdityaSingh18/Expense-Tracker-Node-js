653eec2af33a985d1480dbbfe61c8409a624a50b
Aditya <aditya18.mail@gmail.com>
2022-12-08 19:14:12 +0530

    BCrypt added

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/users')
+const bcrypt = require('bcrypt')
 
 exports.signup=async(req,res)=>{
     console.log(req.body)
@@ -8,14 +9,17 @@ try{
     if(name==undefined||name.length===0||password==undefined||password.length===0||email==undefined||email.length===0){
         return res.status(400).json({err:'bad parameter'})
     }
-
+    
+   bcrypt.hash(password,10,async (err,hash)=>{
     const user = User.findAll({where:{email}})
     if(user.length>0){
         return res.status(409).json({message:'user already exist'})
     }
-    await User.create({name,email,password}).then(()=>{
+    await User.create({name,email,password:hash}).then(()=>{
         res.status(201).json({message:'successfully created new user'})
     })
+   })
+    
     
 }
 catch (err) {
@@ -30,15 +34,18 @@ exports.signin= async(req,res)=>{
         return res.status(400).json({err:'bad parameter'})
     }
     const userMail= await User.findAll({where:{email}})
-    if(userMail.length==0){
-        return res.status(404).json({message:'User not Found'})
-    }
-    if(userMail[0].password==password){
-        return res.status(201).json({message:'login success'})
-    }
-    if(userMail[0].password!=password){
-       return res.status(401).json({message:'User Not Authorized'})
-        
-    }
+bcrypt.compare(password,userMail[0].password,(err,data)=>{
+if(err){
+    return res.status(500).json({message:'something went wrong'})
+}
+if(data===true){
+    return res.status(201).json({message:'login success'})
+    
+
+}
+else{
+    return res.status(400).json({message:'Password is Incorrect'})
+}
+})
 
 }
