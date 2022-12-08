942167cb580987b415209a0233dd2611cf77ed9d
Aditya <aditya18.mail@gmail.com>
2022-12-08 18:19:49 +0530

    login api added

--- controllers/user.js
+++ controllers/user.js
@@ -22,3 +22,23 @@ catch (err) {
     res.status(500).json(err);
 }
 }
+
+exports.signin= async(req,res)=>{
+    console.log(req.body)
+    const {email,password}=req.body;
+    if(password==undefined||password.length===0||email==undefined||email.length===0){
+        return res.status(400).json({err:'bad parameter'})
+    }
+    const userMail= await User.findAll({where:{email}})
+    if(userMail.length==0){
+        return res.status(404).json({message:'User not Found'})
+    }
+    if(userMail[0].password==password){
+        return res.status(201).json({message:'login success'})
+    }
+    if(userMail[0].password!=password){
+       return res.status(401).json({message:'User Not Authorized'})
+        
+    }
+
+}
--- public/login.js
+++ public/login.js
@@ -13,17 +13,26 @@ async function savetocloud(event){
   
   const response= await axios.post("http://localhost:3000/user/login",signinDetails)
   .then((Response)=>{
-    console.log(Response)
-    if(response.status===201){
+    if(Response.status===201){
       console.log(Response)
       alert('login sucessfull')
     }
+
+    if(Response.status===401){
+        console.log(Response)
+        alert('user not authorized')
+      }
+
+      if(Response.status===404){
+        console.log(Response)
+        alert('User not found')
+      }
     else{
         throw new Error('Failed to Login')
     }
   
   })
-  re
+  
   .catch((err)=>{
       document.body.innerHTML+=`div style="color:red;">${err}<div>`
   })
